5b9c516de69b72c791af6ca4eeb043acad43c379
Nico Dinata <ncdnt@icloud.com>
2018-12-17 21:31:38 +1100

    Decompose parsing functionality

--- day_1/get_freq.py
+++ day_1/get_freq.py
@@ -2,15 +2,16 @@ def day_1(file_object):
     total_freq = 0
 
     for line in file_object:
-        curr_sign = line.strip()[0]
-        curr_freq = int(line.strip()[1:])
-
-        if curr_sign == '+':
-            total_freq += curr_freq
-        else:
-            total_freq -= curr_freq
+        total_freq += parse_frequency(line)
 
     print('Total frequency of input (part 1): ' + str(total_freq))
+
+def parse_frequency(line):
+    curr_sign = line.strip()[0]
+    curr_freq = int(line.strip()[1:])
+
+    freq = curr_freq if curr_sign == '+' else (-1 * curr_freq)
+    return freq
 
 
 if __name__ == "__main__":
