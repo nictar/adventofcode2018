3c508b4e06792e0c5c946952458d1e6009b27229
Nico Dinata <ncdnt@icloud.com>
2018-12-18 18:16:52 +1100

    Add day18 input file and stub

---
+++ day18.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+def day_18(fl):
+    acres = defaultdict(int)
+
+    for line in fl:
+        for acre in line.strip():
+            acres[acre] += 1
+
+    print(acres)
+
+if __name__ == "__main__":
+    filename = 'day18_input.txt'
+    fl = open(filename, 'r')
+    day_18(fl)
+    fl.close()
