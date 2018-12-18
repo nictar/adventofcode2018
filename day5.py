dcbd43c6bbd4b9eacf39c86a5b57a8ab57efa57c
Nico Dinata <ncdnt@icloud.com>
2018-12-18 18:58:24 +1100

    Add day5 part1 answer

--- day5.py
+++ day5.py
@@ -1,9 +1,20 @@
 def day_5(fl):
-    pass
+    data = fl.read().strip('\n')
+    answer = ['.']
+
+    for unit in data:
+        x = answer[-1]
+        if unit.swapcase() == x:
+            answer.pop()
+        else:
+            answer.append(unit)
+
+    print('Part 1: ' + str(len(answer)-1))
 
 
 if __name__ == "__main__":
     filename = 'day5_input.txt'
+    # filename = 'test_input.txt'
     fl = open(filename, 'r')
     day_5(fl)
     fl.close()
