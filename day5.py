c02cc5e8c65dd82ae536cb943b231e6e9cc0cd78
Nico Dinata <ncdnt@icloud.com>
2018-12-18 19:20:34 +1100

    Add day5 part2 answer

--- day5.py
+++ day5.py
@@ -1,20 +1,29 @@
 def day_5(fl):
     data = fl.read().strip('\n')
+    print('Part 1: ' + str(react_polymer(data)))
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    # courtesy of /u/andreyrmg (holy cow)
+    shortest = min(react_polymer(unit for unit in data if unit.lower() != letter) for letter in alphabet)
+    print('Part 2: ' + str(shortest))
+
+
+def react_polymer(string):
     answer = ['.']
 
-    for unit in data:
+    for unit in string:
         x = answer[-1]
         if unit.swapcase() == x:
             answer.pop()
         else:
             answer.append(unit)
-
-    print('Part 1: ' + str(len(answer)-1))
+    
+    return len(answer) - 1
 
 
 if __name__ == "__main__":
     filename = 'day5_input.txt'
-    # filename = 'test_input.txt'
     fl = open(filename, 'r')
     day_5(fl)
     fl.close()
