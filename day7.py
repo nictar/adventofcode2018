ce16a166319cd4762d981f94a156cda6fe28c475
Nico Dinata <ncdnt@icloud.com>
2018-12-20 14:46:08 +1100

    Add day7 input file and stub

---
+++ day7.py
@@ -0,0 +1,10 @@
+def day_7(fl):
+    print('Part 1: ')
+    print('Part 2: ')
+
+
+if __name__ == "__main__":
+    filename = 'day7_input.txt'
+    fl = open(filename, 'r')
+    day_7(fl)
+    fl.close()
