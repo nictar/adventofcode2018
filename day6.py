4393e71021685c1c2d063faf7180ac8757124a44
Nico Dinata <ncdnt@icloud.com>
2018-12-20 14:26:20 +1100

    Add day6 part1 full answer

--- day6.py
+++ day6.py
@@ -3,48 +3,41 @@ from collections import defaultdict
 
 def day_6(fl):
     # parse and store coordinate data
-    coordinates = defaultdict(int)
-    for i, row in enumerate(fl):
-        x, y = row.split(', ')
-        coordinates[(int(x), int(y))] = i+1
+    coordinates = []
+    for row in fl:
+        x,y = row.strip().split(', ')
+        coordinates.append((int(x), int(y)))
 
-    # determine the borders of the grid
-    min_x = min(x for x,y in coordinates.keys())
-    min_y = min(y for x,y in coordinates.keys())
-    max_x = max(x for x,y in coordinates.keys())
-    max_y = max(y for x,y in coordinates.keys())
+    # determine the 'size' of the grid
+    max_x = max(x for x,y in coordinates)
+    max_y = max(y for x,y in coordinates)
 
-    # keep a record of the 'infinite' coordinates
-    infinites = [-1]
-    for k,v in coordinates.items():
-        if k[0] == min_x or k[0] == max_x or k[1] == min_y or k[1] == max_y:
-            infinites.append(v)
-
-    # mark each individual location on the grid with the nearest coordinate
-    locations = defaultdict(int)
+    # count the 'area' of each coordinate, taking note of 'infinite' ones
+    areas = defaultdict(int)
+    infinites = [(-1,-1)]
     for x in range(max_x+1):
         for y in range(max_y+1):
-            smallest_dist = sys.maxsize
-            nearest_coor = None
-            for k,v in coordinates.items():
-                curr_dist = dist((x,y), k)
-                if curr_dist < smallest_dist:
-                    smallest_dist = curr_dist
-                    nearest_coor = v
+            nearest = get_nearest((x,y), coordinates)
 
-                # mark this location if it's equidistant to >1 coordinates 
-                elif curr_dist == smallest_dist:
-                    nearest_coor = -1
+            # exclude coordinates that are closest to infinite points
+            if x in (0, max_x) or y in (0, max_y):
+                infinites.append(nearest)
+                if nearest in areas:
+                    del areas[nearest]
+            else:
+                areas[nearest] += 1
 
-            locations[(x, y)] = nearest_coor
+    print(f'Part 1: {max(v for k,v in areas.items() if k not in infinites)}')
 
-    # calculate area of each non-infinite coordinate
-    areas = defaultdict(int)
-    for v in locations.values():
-        if v not in infinites:
-            areas[v] += 1
 
-    print(f'Part 1: {max(v for v in areas.values())}')
+def get_nearest(point, coordinates):
+    """Returns the nearest coordinate to `point` from the list of coordinates."""
+    n = [(dist(point, c), c) for c in coordinates]
+    n.sort()
+
+    if n[0][0] < n[1][0]:
+        return n[0][1]
+    return (-1,-1)
 
 
 def dist(c1, c2):
