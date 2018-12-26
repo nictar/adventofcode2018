58c439a2c78348cc68709253c110bcfa4b753b9d
Nico Dinata <ncdnt@icloud.com>
2018-12-27 10:51:33 +1100

    Add day14 part2 anwer

--- day14.py
+++ day14.py
@@ -5,12 +5,13 @@ def day14():
     elf1 = 0
     elf2 = 1
 
-    while len(scoreboard) < PUZZLE_INPUT + 10:
+    while str(PUZZLE_INPUT) not in scoreboard[-7:]:
         scoreboard += str(int(scoreboard[elf1]) + int(scoreboard[elf2]))
         elf1 = (elf1 + 1 + int(scoreboard[elf1])) % len(scoreboard)
         elf2 = (elf2 + 1 + int(scoreboard[elf2])) % len(scoreboard)
-    
+
     print(f'Part 1: {scoreboard[PUZZLE_INPUT:PUZZLE_INPUT+10]}')
+    print(f'Part 2: {scoreboard.index(str(PUZZLE_INPUT))}')
 
 
 if __name__ == "__main__":
