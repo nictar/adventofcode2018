b15f77301d0177be4b6100e747b2e36e945dd6d5
Nico Dinata <ncdnt@icloud.com>
2019-01-03 01:20:26 +1100

    Parse all the carts on the grid

--- solutions/day13.py
+++ solutions/day13.py
@@ -1,8 +1,33 @@
 def day13(fl):
-    pass
+    carts = []
+    for y, row in enumerate(fl):
+        for x, col in enumerate(row):
+            if col in "^v<>":
+                direction = {
+                    "^": "up",
+                    "v": "down",
+                    "<": "left",
+                    ">": "right"
+                }[col]
+                carts.append(Cart((x, y), direction))
+
+    for cart in carts:
+        print(cart.coordinate)
+
+
+class Cart:
+    def __init__(self, coordinate, direction):
+        self.coordinate = coordinate
+        self.direction = direction
+
+    def move(self):
+        pass
+
+    def turn(self):
+        pass
 
 
 if __name__ == "__main__":
-    with open('input_files/day13_input.txt') as fl:
-    # with open('test_input.txt') as fl:
+    # with open('input_files/day13_input.txt') as fl:
+    with open('solutions/test_input.txt') as fl:
         day13(fl)
